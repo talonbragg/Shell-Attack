efdf588d7d0bcd015109aec340e4b3012197e18b
Talon Bragg <pvpgamecrash@gmail.com>
2018-08-14 13:30:15 -0700

    Update attack.py

--- attack.py
+++ attack.py
@@ -11,7 +11,7 @@ intro_text = """
                                                                                     
 """
 
-credits_text = "© Talon Bragg 2018"
+credits_text = "© Talon Bragg 2018 under the MIT License"
 
 class printcolors:
     HEADER = '\033[95m'
@@ -25,7 +25,7 @@ class printcolors:
 
 correct_password = 0
 print (printcolors.OKGREEN + intro_text + printcolors.ENDC)
-print("\n" + printcolors.OKBLUE + credits_text + printcolors.ENDC)
+print("\n" + printcolors.OKBLUE + credits_text + printcolors.ENDC + "\n")
 hostname = input(printcolors.OKBLUE + 'hostname: ' + printcolors.ENDC)
 username = input(printcolors.OKBLUE + 'username: ' + printcolors.ENDC)
 wordlist = input(printcolors.OKBLUE + 'wordlist: ' + printcolors.ENDC)
